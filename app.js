27c0744194ed4a4edf02a2e99885e054f50b1ffb
Rohaan <rohaanansari321@gmail.com>
2021-07-21 09:47:25 +0530

    changed PORT

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const app = express();
 
 app.use(cors());
 
-const PORT = 5000;
-
 mongoose
   .connect(MONGOURI, {
     useNewUrlParser: true,
@@ -52,50 +50,8 @@ app.use(userRoutes);
 app.use(ConversationRoutes);
 app.use(messagesRoutes);
 
-var server = app.listen(PORT, () => {
+var PORT = process.env.PORT || 5000;
+
+var server = app.listen(PORT || process.env.port, () => {
   console.log('Server listening on port', PORT);
 });
-
-// const io = require('socket.io')(server, {
-//   cors: {
-//     origin: '*',
-//     methods: ['GET', 'POST'],
-//   },
-// });
-
-// let users = [];
-
-// const addUser = (userId, socketId) => {
-//   !users.some((user) => user.userId === userId) &&
-//     users.push({ userId, socketId });
-// };
-
-// const removeUser = (socketId) => {
-//   users = users.filter((user) => user.socketId !== socketId);
-// };
-
-// const getUser = (userId) => {
-//   return users.find((user) => user.userId === userId);
-// };
-
-// io.on('connection', (socket) => {
-//   console.log('A user connected');
-
-//   socket.on('addUser', (userId) => {
-//     addUser(userId, socket.id);
-//     io.emit('getUsers', users);
-
-//     // sending messages
-//     socket.on('sendMessage', ({ senderId, receiverId, text }) => {
-//       console.log(users);
-//       const user = getUser(receiverId);
-//       io.to(user.socketId).emit('getMessage', { senderId, text });
-//     });
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log('A user Disconnected');
-//     removeUser(socket.id);
-//     io.emit('getUsers', users);
-//   });
-// });
